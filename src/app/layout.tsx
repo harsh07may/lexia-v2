7968b58c83d0d27368b9c1b0e8502414c9b3799b
harsh07may <harsh07may@gmail.com>
2025-04-22 23:33:29 +0530

    feat: add favicon and web app icons; update layout for Open Graph and manifest integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,11 @@ const font = Nunito({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Lexia | Language Learning",
   description: "Learn new languages at your own pace.",
+  openGraph: {
+    title: "Lexia | Language Learning",
+    description: "Learn new languages at your own pace.",
+    url: "https://lexia-lang.vercel.app",
+  },
 };
 
 export default function RootLayout({
@@ -21,6 +26,26 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className="hydrated antialiased">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <SessionProvider>
         <body
           className={cn(
