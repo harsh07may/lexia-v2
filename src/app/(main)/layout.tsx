d83dd430a288ef224c30ddb6fd871700b3b06c26
harsh07may <harsh07may@gmail.com>
2025-04-23 00:21:08 +0530

    feat: refactor MainLayout and LessonLayout to include session authentication

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,16 +1,22 @@
 // import Sidebar from "@/components/shared/sidbar";
 import { MobileHeader, Sidebar } from "@/components/shared";
+import { auth } from "@/server/auth";
+import { redirect } from "next/navigation";
 import type { PropsWithChildren } from "react";
 
-const MainLayout = ({ children }: PropsWithChildren) => (
-  <>
-    <MobileHeader />
-    <Sidebar className="hidden md:flex" />
+const MainLayout = async ({ children }: PropsWithChildren) => {
+  const session = await auth();
+  if (!session) redirect("/");
+  return (
+    <>
+      <MobileHeader />
+      <Sidebar className="hidden md:flex" />
 
-    <main className="h-full pt-[60px] md:pt-0 md:pl-[225px] lg:pl-[256px]">
-      <div className="mx-auto h-full max-w-[1056px]">{children}</div>
-    </main>
-  </>
-);
+      <main className="h-full pt-[60px] md:pt-0 md:pl-[225px] lg:pl-[256px]">
+        <div className="mx-auto h-full max-w-[1056px]">{children}</div>
+      </main>
+    </>
+  );
+};
 
 export default MainLayout;
--- src/app/lesson/layout.tsx
+++ src/app/lesson/layout.tsx
@@ -1,6 +1,10 @@
+import { auth } from "@/server/auth";
+import { redirect } from "next/navigation";
 import { type PropsWithChildren } from "react";
 
-const LessonLayout = ({ children }: PropsWithChildren) => {
+const LessonLayout = async ({ children }: PropsWithChildren) => {
+  const session = await auth();
+  if (!session) redirect("/");
   return (
     <div className="flex h-screen flex-col">
       <div className="flex h-full w-full flex-col gap-6">{children}</div>
